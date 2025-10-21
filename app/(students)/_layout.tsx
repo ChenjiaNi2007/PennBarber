78a62c39aff75076f8c1a5d9c35cad1f63caedca
Chenjia Ni <Jeffernickel2007@gmail.com>
2025-10-20 20:21:28 -0400

    Explore page that retreives barber list Student side

--- app/(students)/_layout.tsx
+++ app/(students)/_layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout() {
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor:"coral"}}>
       <Tabs.Screen name="index" options={{ title: "Home", tabBarIcon: ({ color }) => (<AntDesign name="home" size={24} color={color} />) }} />
-      <Tabs.Screen name="login" options={{ title: "Login" }} />
+      <Tabs.Screen name="explore" options={{ title: "Explore Page" }} />
     </Tabs>
   );
 }
---
+++ app/(students)/explore.tsx
@@ -0,0 +1,40 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { BARBER_COLLECTION_ID, DATABASE_ID, tablesDB } from "@/lib/appwrite";
+import { useEffect, useState } from "react";
+import Profile from "@/lib/profile";
+
+export default function LoginScreen(){
+
+    const [profiles, setProfiles] = useState<any>();
+    const [loading, setLoading] = useState<Boolean>(true)
+    
+    const getProfiles = async () => {
+        try {
+            const result = await tablesDB.listRows({
+                databaseId: DATABASE_ID!,
+                tableId: BARBER_COLLECTION_ID!
+            });
+            console.log(result.rows)
+            setProfiles(result.rows);
+        } finally {
+            setLoading(false)
+        }
+        
+    }
+
+    useEffect(() => {getProfiles()},[]);
+
+    if(loading) {
+        return <Text>Loading...</Text>
+    }
+
+    return (
+        <View>
+            <Text>Hello this is the Explore Page</Text>
+            {profiles.map((profile: any) => {
+                return <Profile profile={profile}></Profile>
+            })}
+        </View>
+    );
+}
